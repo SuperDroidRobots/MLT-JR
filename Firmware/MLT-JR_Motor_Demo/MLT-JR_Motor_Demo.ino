7b730d0e06237c13e5923f55194455d6f4c81a2c
Jason Traud <Jason@SDRobots.com>
2015-05-23 10:36:20 -0400

    Added additional comments and changed software serial ports
    Expanded the header and program flow comments to be more clear and
    included hardware pin connections.
    
    Changed the software serial port from pin 49 to pin 13 so that the same
    firmware will work on the Uno and the Mega without alterations. The
    platform ships with either microcontroller depending on the options
    selected during checkout.

--- Firmware/MLT-JR_Motor_Demo/MLT-JR_Motor_Demo.ino
+++ Firmware/MLT-JR_Motor_Demo/MLT-JR_Motor_Demo.ino
@@ -3,13 +3,25 @@
 
    MLT-JR Motor Movement Demonstration
    AUTHOR: Jason Traud
-   DATE: February 23, 2015
+   DATE: May 23, 2015
    
-   This firmware serves as a movement demonstration for the MLT-JR Tracked Platform
+   This firmware serves as a movement demonstration for the MLT-JR Tracked Platform. The 
+   Arduino communicates with the Sabertooth motor controller over a software serial port.
+   The commands are sent by the Sabertooth library when the .drive() and .turn() functions
+   are called. Both of these commands must be sent together so that the motor controller 
+   can calculate the appropriate motor mixing. If one is sent without the other the motor
+   controller will not respond.   
    
-   Hardware: Arduino MegaR3      (MCU-049-000)
+   
+   Hardware: Arduino Uno R3      (MCU-050-000)
              MLT-JR Programmable (TP-220-002) 
              Sabertooth 2x12     (TE-091-212)
+
+   Connections: 
+   
+     Arduino D13  -   Sabertooth S1 (This is the software serial port)
+     Arduino D12  -   Sabertooth S2 (This is the optional emergency stop port)
+     Arduino Gnd  -   Sabertooth 0V (A common ground is needed for stable communication) 
 			
    License: CCAv3.0 Attribution-ShareAlike (http://creativecommons.org/licenses/by-sa/3.0/)
    You're free to use this code for any venture. Attribution is greatly appreciated. 
@@ -27,9 +39,10 @@
 // ****************************************************
 // Motor Controller
 // ****************************************************
-SoftwareSerial SWSerial(NOT_A_PIN, 49); // RX on no pin (unused), TX on pin 11 (to S1).
+SoftwareSerial SWSerial(NOT_A_PIN, 13); // RX on no pin (unused), TX on pin 13 (to S1).
 Sabertooth ST(128, SWSerial); // Address 128, and use SWSerial as the serial port.
 
+#define SAB_ESTOP 12
 
 // ****************************************************
 // Initial setup function, called once
@@ -37,8 +50,19 @@ Sabertooth ST(128, SWSerial); // Address 128, and use SWSerial as the serial por
 // ****************************************************
 void setup()
 {
-  SWSerial.begin(9600);
-  ST.autobaud();
+  SWSerial.begin(9600);      // Serial communicaiton to the motor controller
+  
+  pinMode(SAB_STOP, OUTPUT); // Our emergency stop signal
+  
+  delay(200);                // Give the motor controller time to boot 
+  
+  ST.autobaud();             // The motor controller configures it's baud rate to 
+                             // match the first character sent to it. The autobaud
+                             // function sends 0xAA (0b 1010 1010). 
+                            
+  
+  // Release the emergency stop
+  digitalWrite(SAB_ESTOP, HIGH);                            
 }
 
 void loop()
@@ -48,27 +72,37 @@ void loop()
   // NOTE: The below drive and turn commands must be called as shown below. The motor controller
   // will not act on a drive and turn command unless both are sent.
   
-  // Ramp up drive forward to half speed
+  // NOTE: The drive and turn commands expect a range of -127 (full speed reverse)
+  // to 127 (full speeed foward), with a value of 0 being stopped. 
+  
+  // Forward movement demonstration
+  // ****
+  // Ramp up drive forward to half speed but keep turn as 0
   for (power = 0; power <= 64; power ++) {
     ST.drive(power);
     ST.turn(0);
     delay(20);
   }
 
-  // Ramp drive forward half speed to drive backward half speed
+  // Reverse movement demonstration
+  // ****
+  // Ramp drive from half speed foward to half speed backward
   for (power = 64; power >= -64; power --) {
     ST.drive(power);
     ST.turn(0);
     delay(20);
   }
 
-  // Ramp down drive backward until stopped  
+  // Complete the forward and back demonstration by ramping
+  // reverse speed back down to 0 (stopped)
   for (power = -64; power <= 0; power ++) {
     ST.drive(power);
     ST.turn(0);
     delay(20);
   }
-
+ 
+  // Turn Demonstration
+  // ***
   // Ramp up turn right to half speed
   for (power = 0; power <= 64; power ++) {
     ST.drive(0);
